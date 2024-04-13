6f437e1dc3da006e87db8ca303c8a8bd7ed485f1
David Gilles <114345154+d-gilles@users.noreply.github.com>
2024-04-13 15:36:59 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -1,5 +1,6 @@
-touch entrypoint 
-cd ..
-touch clonehere
+cd /home/src
 git clone https://github.com/d-gilles/energy_data_etl.git
-
+cd energy_data_etl
+pip install -r requirements.txt
+cd /home/src
+mage start energy_data_etl
